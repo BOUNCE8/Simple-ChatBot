cb87a09ac18e70353c19f2bea85acfade99ae6d2
BOUNCE8 <43513640+BOUNCE8@users.noreply.github.com>
2020-10-10 15:41:23 +0100

    minor changes
    made it clearer what is is jquery collection and dom nodes by prepending '$'

--- JS/function.js
+++ JS/function.js
@@ -10,22 +10,22 @@
       ];
 
       const catchOutputs = [
-          ["Hello", "Hey", "Hi there!", "what's good?"],
-          ["Not bad thanks, how are you doing?", "great, how are you feeling?", "Amazing, what about you?"],
-          ["Not much", "So much I can't remember", "working on projects"],
-          ["That's good", "Good to hear", "Great"],
-          ["Sorry to hear that", "That's a shame"],
-          ["People are crazy"],
-          ["No problem", "Pleasure is mine", "You're welcome"],
-          ["Take care", "Goodbye", "See you later"]
+          ["hello", "hey", "hi there!", "what's good?"],
+          ["not bad thanks, how are you doing?", "great, how are you feeling?", "amazing, what about you?"],
+          ["not much", "so much I can't remember", "working on projects"],
+          ["that's good", "good to hear", "great"],
+          ["sorry to hear that", "that's a shame"],
+          ["people are crazy"],
+          ["no problem", "pleasure is mine", "you're welcome"],
+          ["take care", "goodbye", "see you later"]
       ];
 
       const randomOutputs = [
-          "Interesting",
-          "I'm listening",
-          "Tell me more",
-          "That's Tricky",
-          "Oh yeee"
+          "interesting...",
+          "i'm listening",
+          "tell me more",
+          "that's tricky",
+          "oh yeah?"
       ];
 
       window.onload = function() {
@@ -35,10 +35,10 @@
       // on enter press
       $("#input").on("keypress", function(e) {
         const ENTER_KEY = 13;
-        const input = $("#input").val();
+        const $input = $("#input").val();
         if(e.keyCode === ENTER_KEY){
-          if(input != "") {
-            output(input);
+          if($input != "") {
+            output($input);
             $('#input').val("");
           }
         }
@@ -46,35 +46,36 @@
       
       // on click send
       $("#send-btn").on("click", function(e) {
-          const input = $("#input").val();
-          if(input != "") {
-            output(input);
+          const $input = $("#input").val();
+          if($input != "") {
+            output($input);
             $('#input').val("");
           }
       });
 
-      //decider function
-      output = (input) => {
+      //compares user input to 'catchInput' array, if it finds a match then run compare function. If not return 'randomOutputs' 
+      output = ($input) => {
         const currentDate = new Date()
-        const time = `${formatTime(currentDate.getHours())}:${formatTime(currentDate.getMinutes())}`
+        const $time = `${formatTime(currentDate.getHours())}:${formatTime(currentDate.getMinutes())}`
         let result
-        let text = input.toLowerCase()
+        let $text = $input.toLowerCase().replace(/[^\d\w\s]/gi, "");
 
-        if (compare(catchInputs, catchOutputs, text)){
-          result = compare(catchInputs, catchOutputs, text);
+        if (compare(catchInputs, catchOutputs, $text)){
+          result = compare(catchInputs, catchOutputs, $text);
         } else {
           result = randomOutputs[Math.floor(Math.random() * randomOutputs.length)]
         }
 
-        bot(result, time, scrollDown())
-        user(text, time, scrollDown()) 
+        bot(result, $time, scrollDown())
+        user($input, $time, scrollDown()) 
           
       }
 
-      function formatTime(time) {
-          return time < 10 ? (`0${time}`) : time;
+      function formatTime($time) {
+          return $time < 10 ? (`0${$time}`) : $time;
       }
-        
+      
+      // takes the matched user input then returns a response from the 'botArray' array with the same initial index of the 'userArray' then returns a random response from that array (botArray[0-?][random])
       compare = (userArray, botArray, string) => {
         let item;
         for (let a = 0; a < userArray.length; a++) {
@@ -88,15 +89,16 @@
         return item;
       }
 
-      user = (text, time) => {
-          text = text.charAt(0).toUpperCase() + text.substring(1)
-          $('#chat-container').append(`<div class="outerBubbleUser"><div class="textBuble user"><p>${text}</p></div><span class="timeStamp sml-margin-l"><p>${time}</p></span></div>`);
+      user = ($input, $time) => {
+          $input = $input.charAt(0).toUpperCase() + $input.substring(1)
+          $('#chat-container').append(`<div class="outerBubbleUser"><div class="textBuble user"><p>${$input}</p></div><span class="timeStamp sml-margin-l"><p>${$time}</p></span></div>`);
         }
 
-      bot = (result, time) => {
+      bot = (result, $time) => {
+        result = result.charAt(0).toUpperCase() + result.substring(1)
         setTimeout(function(){
-          $('#chat-container').append(`<div class="outerBubbleBot"><span class="timeStamp sml-margin-r"><p>${time}</p></span><div class="textBuble bot"><p>${result}</p> </div></div>`)
+          $('#chat-container').append(`<div class="outerBubbleBot"><span class="timeStamp sml-margin-r"><p>${$time}</p></span><div class="textBuble bot"><p>${result}</p> </div></div>`)
         }, 1000);
         }
 
-      scrollDown = () => $('#chat-container').animate({scrollTop:1000000}, 800)
+      scrollDown = () => $('#chat-container').animate({scrollTop:100000}, 800)
